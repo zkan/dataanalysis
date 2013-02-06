8fa3a351b1c3670299c2c6a1346f0e3e27ef0ba8
Kan O <zkan.cs@gmail.com>
2013-02-06 21:03:21 +0700

    - Add an example code for PCA and SVD - Update the clustering code - Add an assignment folder

--- code/clustering_example.R
+++ code/clustering_example.R
@@ -10,11 +10,24 @@ distxy <- dist(dataFrame)
 hClustering <- hclust(distxy)
 plot(hClustering)
 
+# myplclust is the function provided in the course
 myplclust(hClustering, lab = rep(1:3, each = 4), lab.col = rep(1:3, each = 4))
+
+kmeansObj <- kmeans(dataFrame, centers = 3)
+names(kmeansObj)
+
+par(mar = rep(0.2, 4))
+plot(x, y, col = kmeansObj$cluster, pch = 19, cex = 2)
+points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
 
 set.seed(143)
 dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
 heatmap(dataMatrix)
+
+kmeansObj2 <- kmeans(dataMatrix, centers = 3)
+par(mfrow = c(1, 2), mar = rep(0.2, 4))
+image(t(dataMatrix)[, nrow(dataMatrix):1], yaxt = "n")
+image(t(dataMatrix)[, order(kmeansObj$cluster)], yaxt = "n")
 
 
 
---
+++ code/dimension_reduction_example.R
@@ -0,0 +1,44 @@
+set.seed(12345)
+par(mar = rep(0.2, 4))
+dataMatrix <- matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(dataMatrix)[, nrow(dataMatrix):1])
+
+# Cluster the data
+par(mar = rep(0.2, 4))
+heatmap(dataMatrix)
+
+# Add a pattern 
+set.seed(678910)
+for(i in 1:40) {
+    # flip a coin
+    coinFlip <- rbinom(1, size = 1, prob = 0.5)
+    # if coin is heads add a common pattern to that row
+    if(coinFlip) {
+        dataMatrix[i, ] <- dataMatrix[i, ] + rep(c(0, 3), each = 5)
+    }
+}
+
+par(mar = rep(0.2, 4)
+heatmap(dataMatrix)
+
+hh <- hclus(dist(dataMatrix))
+dataMatrixOrdered <- dataMatrix[hh$order, ]
+plot(rowMeans(dataMatrixOrder), 40:1, , xlab = "Row", ylab = "Row Mean", pch = 19)
+plot(colMeans(dataMatrixOrder), xlab = "Column", ylab = "Column Mean", pch = 19)
+
+svd1 <- svd(scale(dataMatrixOrdered))
+names(svd1)
+pca1 <- prcomp(dataMatrixOrdered, scale = TRUE)
+plot(pca1$rotaion[, 1], svd1$v[, 1], pch = 19)
+
+# one way to deal with missing values -- use imputing
+library(impute)
+dataMatrix2 <- dataMatrixOrdered
+dataMatrix2[sample(1:100, size = 40, replace = F)] <- NA
+dataMatrix2 <- impute.knn(dataMatrix2)$data
+svd1 <- svd(scale(dataMatrixOrdered))
+svd2 <- svd(scale(dataMatrix2))
+par(mfrow = c(1, 2))
+plot(svd1$v[, 1], pch = 19)
+plot(svd2$v[, 1], pch = 19)
+
